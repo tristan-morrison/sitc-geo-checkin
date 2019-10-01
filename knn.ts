910ae89ce980bea6cfd3107208d5247f8bacf224
Tristan Lopus <tristan.lopus@gmail.com>
2019-10-01 08:59:44 -0700

    first commit

---
+++ knn.ts
@@ -0,0 +1,14 @@
+/**
+ * K-Nearest Neighbor functions.
+ */
+
+import kdbush from "kdbush";
+import geokdbush from "geokdbush";
+
+/**
+ * Given a set of points, return the subset that are no more than a given distance away from the given reference point
+ * @param thresholdRange  The maximum distance allowed between a point in the result set and the reference point
+ */
+function filterPointsByThresholdRange(refPt, searchPts, thresholdRange) {
+    
+}
